dbe7c8b376df0df64d7067f57898f7b15d2a2d32
Ismael Sane <six.echo.sa@gmail.com>
2021-04-26 19:22:06 +0200

    Declare global variables

--- index.rb
+++ index.rb
@@ -15,3 +15,21 @@ $payroll_records = [ {EMPLOYEE_NUMBER: '0012345', EMPLOYEE_NAME: 'JOE SUDDS',
 
                     {EMPLOYEE_NUMBER: '0067890', EMPLOYEE_NAME: 'JAMES FRITZ',
                      HOURLY_RATE: '$21.00', HOURS_WORKED: '50.50'} ]
+$overtime_pay = 0
+$regular_pay = 0
+$gross_pay = 0
+$tax_withheld = 0
+$net_pay = 0
+$hourly_pay_rate = 0
+$hours_worked = 0
+$regular_pay_total = 0
+$overtime_pay_total = 0
+$gross_pay_total = 0
+$tax_withheld_total = 0
+$net_pay_total = 0
+
+$employee_number = ''
+$employee_name = ''
+
+$line_width = 135
+$count = 0
