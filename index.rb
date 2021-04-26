6780d8c88ea7a54269e787108eb1732eeea578d2
Ismael Sane <six.echo.sa@gmail.com>
2021-04-26 19:20:06 +0200

    Create a data set

---
+++ index.rb
@@ -0,0 +1,17 @@
+$payroll_records = [ {EMPLOYEE_NUMBER: '0012345', EMPLOYEE_NAME: 'JOE SUDDS',
+                    HOURLY_RATE: '$10.34', HOURS_WORKED: '45.50'},
+
+                    {EMPLOYEE_NUMBER: '0023456', EMPLOYEE_NAME: 'SUSIE SAMPLE',
+                     HOURLY_RATE: '$12.57', HOURS_WORKED: '40.00'},
+
+                    {EMPLOYEE_NUMBER: '0034567', EMPLOYEE_NAME: 'FRDERICK FENDAHL',
+                     HOURLY_RATE: '$9.75', HOURS_WORKED: '75.25'},
+
+                    {EMPLOYEE_NUMBER: '0045678', EMPLOYEE_NAME: 'PENELOPE PENNEL',
+                     HOURLY_RATE: '$20.98', HOURS_WORKED: '40.00'},
+
+                    {EMPLOYEE_NUMBER: '0056789', EMPLOYEE_NAME: 'HELEN HIGHWATER',
+                     HOURLY_RATE: '$8.35', HOURS_WORKED: '35.00'},
+
+                    {EMPLOYEE_NUMBER: '0067890', EMPLOYEE_NAME: 'JAMES FRITZ',
+                     HOURLY_RATE: '$21.00', HOURS_WORKED: '50.50'} ]
